3365b3ea55b439aa151571ad2f93f7569a934472
rogersebastiany <rogersebastiany@gmail.com>
2023-10-11 19:46:14 -0300

    created docker-compose.yml to run a postgresql server

---
+++ src/main/java/br/com/uol/campaigns/CampaignsApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol.campaigns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uol/campaigns/CampaignsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol.campaigns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
