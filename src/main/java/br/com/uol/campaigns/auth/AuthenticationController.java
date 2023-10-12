da8187cc5f1e4f9ac9a75db94e2a30708319a11f
rogersebastiany <rogersebastiany@gmail.com>
2023-10-12 20:23:07 -0300

    jwt implementation and login logic

---
+++ src/main/java/br/com/uol/campaigns/auth/AuthenticationController.java
@@ -0,0 +1,42 @@
+package br.com.uol.campaigns.auth;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.uol.campaigns.service.TokenService;
+import br.com.uol.campaigns.user.IUserRepository;
+import br.com.uol.campaigns.user.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("")
+    public ResponseEntity login(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
+        } else {
+            if (BCrypt.verifyer().verify(userModel.getPassword().toCharArray(), user.getPassword()).verified) {
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                        tokenService.generateToken(userModel)
+                );
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong password");
+            }
+        }
+
+    }
+}
---
+++ src/main/java/br/com/uol/campaigns/service/TokenService.java
@@ -0,0 +1,51 @@
+package br.com.uol.campaigns.service;
+
+import br.com.uol.campaigns.user.UserModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserModel userModel) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Todolist")
+                    .withSubject(userModel.getUsername())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error generating JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Todolist")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Invalid or expired JWT");
+        }
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
