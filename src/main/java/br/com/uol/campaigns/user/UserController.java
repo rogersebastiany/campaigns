6646a9dffb52ff2f90aee03f3bae4fdfcb2323a2
rogersebastiany <rogersebastiany@gmail.com>
2023-10-12 17:47:24 -0300

    user creation logic and password encryption

--- src/main/java/br/com/uol/campaigns/user/UserController.java
+++ src/main/java/br/com/uol/campaigns/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.uol.campaigns.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +16,17 @@ public class UserController {
     @PostMapping("")
     public ResponseEntity create(@RequestBody UserModel userModel) {
 
+        // tratamento de return clean
         if (this.userRepository.findByUsername(userModel.getUsername()) != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
         }
 
+        // encrypt na senha do usuario
+        userModel.setPassword(
+                BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray())
+        );
+
+        // retorna salvando
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 this.userRepository.save(userModel)
         );
